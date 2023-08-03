41d2e763e35d0d78341bff3acbf3130a3f6b2b64
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2023-08-03 17:59:08 +0700

    v1

---
+++ HIT_Product/main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from PIL import Image
+import os
+import subprocess
+import uuid
+
+
+IMAGEDIR = "imgs/" #tao thu muc images để lưu ảnh lấy được và ảnh generate
+styles_dir = "styles/"
+content_dir = "content_user/"
+generate_dir = "generate/"
+
+def saveFile(content, filename):
+    with open(f"{IMAGEDIR}{content_dir}{filename}", "wb") as f:
+        f.write(content)
+
+
+st.set_page_config(page_title='Hello',page_icon="📷", layout="wide")
+
+
+st.title("Style Transfer")
+
+content_file = st.file_uploader("Upload Content Image")
+if content_file is not None:
+    # Hiển thị ảnh nội dung
+    content_image = Image.open(content_file)
+    st.image(content_image, caption="Content Image", use_column_width=True)
+    for filename in os.listdir(f"{IMAGEDIR}{generate_dir}"):
+       file_path = os.path.join(IMAGEDIR, generate_dir, filename)
+
+       os.remove(file_path)
+
+
+    # Cho phép người dùng chọn kiểu ảnh từ danh sách các kiểu ảnh có sẵn
+    style_options = [f for f in os.listdir("model") if f.endswith(".pth")]
+    style_name = st.selectbox("Select Style", style_options)
+
+    # Nếu người dùng đã chọn kiểu ảnh
+    if style_name:
+        # Hiển thị ảnh kiểu
+        style_img = style_name[:style_name.find(".pth")] + ".jpg"
+        style_image = Image.open(os.path.join("imgs", "styles", f"{style_img[: len(style_img) - 7]}", style_img))
+        st.image(style_image, caption="Style Image", use_column_width=True)
+
+        fileName_rand = uuid.uuid4()
+        filename = f"{fileName_rand}.jpg"
+        content_image.save(os.path.join("imgs", "content_user", filename))
+
+        code = f"python test_main.py  --model_load_path model\{style_name} --test_content imgs\content_user\{filename} --imsize 256 --output imgs\generate\{filename}".split()
+        process = subprocess.run(code, stdout=subprocess.PIPE)
+        # print(process.decode("utf-8"))
+        st.text(process.stdout.decode("utf-8"))
+
+        result_image = Image.open(os.path.join("imgs", "generate", filename))
+        st.image(result_image, caption="Result Image", use_column_width=True)
---
+++ HIT_Product/test.py
@@ -0,0 +1,19 @@
+from images_unit import imsave, imload, get_transformer
+from network import TransformNetwork
+import torch
+from upscale import up_scale_x3_normal_fast
+def load_transform_network(args):
+    transform_network = TransformNetwork()
+    transform_network.load_state_dict(torch.load(args.model_load_path, map_location=torch.device('cpu')))
+    return transform_network
+
+def network_test(args):
+    device = ('cuda' if args.cuda_device_no >= 0 else 'cpu')
+    transform_network = load_transform_network(args).to('cpu')
+
+    input_image = imload(args.test_content, args.imsize)
+    with torch.no_grad():
+        output_image = transform_network(input_image)
+    imsave(output_image, args.output)
+    up_scale_x3_normal_fast(args.output, args.output)
+    return None
