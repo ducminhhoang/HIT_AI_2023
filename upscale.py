3553aa3f253f25d1bedab8bf4fb8b7cff8c916d7
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2023-08-12 23:06:09 +0700

    Update upscale.py

--- upscale.py
+++ upscale.py
@@ -4,7 +4,7 @@ def up_scale_x3_normal_fast(path_image, save_path):
     sr = dnn_superres.DnnSuperResImpl_create()
     image = cv2.imread(path_image)
     print(image.shape)
-    path = "model_upscale\\ESPCN_x3.pb"
+    path = "model_upscale/ESPCN_x3.pb"
     sr.readModel(path)
     sr.setModel("espcn", 3)
     result = sr.upsample(image)
