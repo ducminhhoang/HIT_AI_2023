825d1ce8d8403a58d1b3a70f9cfb5fc85b50d7f2
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2023-08-12 11:03:24 +0700

    Update apii.py

--- apii.py
+++ apii.py
@@ -1,5 +1,5 @@
 import uvicorn
-
+import sys
 from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, Form #import class FastAPI() từ thư viện fastapi
 from pydantic import BaseModel #thu vien giup tu tao doi tuong (neu can)
 from fastapi.responses import FileResponse #
@@ -13,9 +13,9 @@ import subprocess
 from PIL import Image
 import base64
 from io import BytesIO
+import random
 
 import os
-import uuid #giup tao ten ngau nhien 16 ky tu
 
 app = FastAPI() # gọi constructor và gán vào biến app
 security = HTTPBasic()
@@ -28,99 +28,63 @@ app.add_middleware(
     allow_headers=["*"], # Cho phép tất cả các tiêu đề
 )
 
-@app.get("/dev")
-def read_root(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "nstadmin")
-    correct_password = secrets.compare_digest(credentials.password, "123456")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=400,
-            detail="Incorrect usename or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return {"message": ""}
-
-
-
 IMAGEDIR = "imgs/" #tao thu muc images để lưu ảnh lấy được và ảnh generate
 styles_dir = "styles/"
 content_dir = "content_user/"
 generate_dir = "generate/"
-dictStyle_Model = {"style1.jpg" : "William_Turner_13.pth", "style2.jpg" : "Frida_Kahlo.pth"}
 
-def saveFile(content, file: UploadFile = File(...)):
-    with open(f"{IMAGEDIR}{content_dir}{file.filename}", "wb") as f:
-        f.write(content)
 
-@app.post("/trainning")
-async def setTrain(style: UploadFile = File(...), max_iter: int = Form(1000)):
+def removeDir(dir: str):
+    for filename in os.listdir(dir):
+        file_path = os.path.join(dir, filename)
+        os.remove(file_path)
 
-    return {'massage': ''}
-
-@app.get("/getNameStyle")
-async def getName():
-    images = os.listdir("model")
-    print({"images": images})
-    return {"images": images}
 
 @app.get("/getImage")
-async def hello(str: str):
+async def hello():
     images = []
-    for filename in os.listdir("imgs\\"):
-        if filename.endswith(".jpg"):
-            with Image.open(f"imgs/{filename}") as img:
+    for filename in os.listdir(os.path.join("imgs", "styles")):
+            with Image.open(os.path.join("imgs", "styles", filename)) as img:
                 buffered = BytesIO()
+                img = img.convert("RGB")
                 img.save(buffered, format="JPEG")
                 img_str = base64.b64encode(buffered.getvalue()).decode()
+                filename = " ".join(filename.split("_"))
+                filename = filename[:-4]
                 images.append({"name": filename, "data": img_str})
     return {"images": images}
 
-# @app.post("/train")
-# async def setPath(fileStyle : UploadFile = File(...)):
-
 
 @app.post("/upload")
-async def getImage(fileContent: UploadFile = File(...), fileStyle: str = Form(...)):
-    # mỗi lần chạy lại sẽ xóa file ảnh cũ đi
-    print(fileStyle)
-    for filename in os.listdir(f"{IMAGEDIR}{generate_dir}"):
-       file_path = os.path.join(IMAGEDIR, generate_dir, filename)
-
-       os.remove(file_path)
-
-
-    fileName_rand = uuid.uuid4()
-    fileName = fileContent.filename
-    fileContent.filename = f"{fileName_rand}.jpg"
+async def getImage(fileContent: UploadFile = File(...), fileStyle: str = Form(None)):
+    removeDir(os.path.join("imgs", "generate"))
+    removeDir(os.path.join("imgs", "content_user"))
+    fileContent.filename = "tmp.jpg"
     content = await fileContent.read()
-
-    saveFile(content, fileContent)
+    with open(os.path.join("imgs", "content_user", fileContent.filename), "wb") as f:
+        f.write(content)
 
     #code xử lý generate ảnh
-
-    if (fileStyle in dictStyle_Model.keys()):
-        model_path = dictStyle_Model[fileStyle]
-        print(model_path)
-        code = f"python test_main.py  --model_load_path model\{model_path} --test_content imgs\content_user\{fileContent.filename} --imsize 256 --output imgs\generate\{fileName_rand}.jpg".split()
-        print(code)
-        process = subprocess.Popen(code, stdout=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        print(stdout.decode("utf-8"))
-
+    filepath = ""
+    if fileStyle is None:
+        filepath = [f for f in os.listdir(os.path.join("model")) if f.endswith(".pth")]
+        r = random.randint(0, len(filepath))
+        filepath = filepath[r]
     else:
-        raise HTTPException(
-            status_code=400,
-            detail="Not Found style in Database",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    path = f"imgs\\generate\\{fileName_rand}.jpg"
+        filepath = "_".join(fileStyle.split(" ")) + ".pth"
+
+    code = f"{sys.executable} test_main.py  --model_load_path model/{filepath} --test_content imgs/content_user/tmp.jpg --imsize 256 --output imgs/generate/tmp.jpg".split()
+    process = subprocess.Popen(code, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout.decode("utf-8"))
+
+    path = os.path.join("imgs", "generate", "tmp.jpg")
     print(path)
     return FileResponse(path)
 
 
-
 if __name__ == "__apii__":
-    uvicorn.run("apii:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("apii:app", host="0.0.0.0", port=1000, reload=True)
 # uvicorn apii:app --host 0.0.0.0 --port 8000 --reload
 # http://127.0.0.1:8000/
 
